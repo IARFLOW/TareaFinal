5d4425771839368d2a7ee37bf023dea27d8ba81f
Ignacio Ampurdanes <ignaciopercusion@gmail.com>
2025-03-05 19:06:46 +0100

    Seguimos, vamos a por el Nivel 5

--- Assets/Scripts/Character/CameraController.cs
+++ Assets/Scripts/Character/CameraController.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
     private Transform player;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Knight").transform;
+        GameObject playerObject = GameObject.Find("Knight");
+        if (playerObject == null)
+        {
+            playerObject = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        if (playerObject != null)
+        {
+            player = playerObject.transform;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 posicion = player.position;
-        posicion.z = transform.position.z;
-        transform.position = posicion;
-
+        if (player != null)
+        {
+            Vector3 posicion = player.position;
+            posicion.z = transform.position.z;
+            transform.position = posicion;
+        }
     }
 }
